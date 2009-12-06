b316b3b647016425d9aefbd590f8ee7c81aaa155
Dmitry <dmitry@dmitry-laptop.(none)>
2009-12-06 18:42:20 +0200

    Removed 'Translate' button.

--- gsocrat.rb
+++ gsocrat.rb
@@ -7,6 +7,8 @@ require 'json'
 
 
 class Translator
+  GOOGLE_APIS = 'ajax.googleapis.com'
+  TRANSLATE_ERROR = 'Translate error'
   def initialize(lang_from, lang_to)
     @lang_from, @lang_to = lang_from, lang_to
   end
@@ -14,35 +16,42 @@ class Translator
   def translate(string)
     request = "/ajax/services/language/translate?v=1.0&q=#{CGI.escape(string)}&langpair=#{@lang_from}%7C#{@lang_to}"
     begin
-      response = Net::HTTP.get_response('ajax.googleapis.com', request)
-    rescue Net::HTTPError
-      "Can\'t connect to translate server."
+      response = Net::HTTP.get_response(GOOGLE_APIS, request)
+    rescue
+      "Can't connect to #{GOOGLE_APIS}."
     else
       json_response = JSON.parse(response.body)
-      json_response && json_response['responseData'] ? json_response['responseData']['translatedText'] : "Translate error."
+      json_response && json_response['responseData'] ? json_response['responseData']['translatedText'] : TRANSLATE_ERROR
     end
   end
 end
 
 
 word_entry      = Gtk::Entry.new
-button          = Gtk::Button.new("Translate!")
 window          = Gtk::Window.new
 window.title    = "GSocrat"
+window.resizable= false
+
 translator      = Translator.new('en', 'ru')
 responseview    = Gtk::TextView.new
 responseview.editable = false
 
-window.signal_connect("delete_event") { Gtk.main_quit }
-window.signal_connect("key_press_event") { |w, e| Gtk.main_quit if e.keyval == Gdk::Keyval::GDK_Escape }
+window.signal_connect('delete_event') { Gtk.main_quit }
+
+window.signal_connect('key_press_event') do |w, e|
+  case e.keyval
+    when Gdk::Keyval::GDK_Escape
+      Gtk.main_quit
+    when Gdk::Keyval::GDK_Return
+      responseview.buffer.text = translator.translate(word_entry.text)
+  end
+end
 
 main_box = Gtk::VBox.new(false, 5)
 main_box.pack_start_defaults(word_entry)
-main_box.pack_start_defaults(button)
 main_box.pack_start_defaults(responseview)
-
-button.signal_connect("clicked") { responseview.buffer.text = translator.translate(word_entry.text) }
 
 window.add(main_box)
 window.show_all
 Gtk.main
+
